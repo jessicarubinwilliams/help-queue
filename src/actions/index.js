aaf2dc1756b1460e1b465e39f4d5ec728dc79059
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-02-12 14:23:11 -0800

    Add code to pass test of addTicket action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,5 +8,12 @@ export const toggleForm = () => ({
 });
 
 export const addTicket = (ticket) => {
-  
+  const { names, location, issue, id } = ticket;
+  return {
+    type: 'ADD_TICKET',
+    names: names,
+    location: location,
+    issue: issue,
+    id: id
+  }
 }
